8c406a5e7036e139bf5c46d1034da882c0c7495d
root <root@MAY.localdomain>
2025-10-26 20:16:43 +0100

    Update on the online bookstore.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,5 +1,7 @@
--- Dropping existing tables to prevent errors
-DROP TABLE IF EXISTS Authors;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+USE alx_book_store;
+
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
